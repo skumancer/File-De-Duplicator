e63adc1f97586d6686242b54761df5567b86ddf5
Ricardo Chavarria <skumancer@gmail.com>
2021-02-26 23:28:24 -0500

    Initial Commit

---
+++ Shared/File_De_DuplicatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  File_De_DuplicatorApp.swift
+//  Shared
+//
+//  Created by Ricardo Chavarria on 02/26/21.
+//
+
+import SwiftUI
+
+@main
+struct File_De_DuplicatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
